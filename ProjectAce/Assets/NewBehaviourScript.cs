31b61215f953850550bbe73458a073315e1f6b43
김태형 <xo052345@gmail.com>
2020-12-12 21:47:59 +0900

    [기획/김태형] 버튼 클릭시 플레이어 화면 전환

---
+++ ProjectAce/Assets/NewBehaviourScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject Player1;
+    public GameObject Player2;
+    public Button UIButton;
+    private bool PlayerSwitch;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player1.SetActive(true);
+        Player2.SetActive(false);
+
+        PlayerSwitch = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTextButtonCLick()
+    {
+        if(PlayerSwitch == true)
+        {
+            PlayerSwitch = false;
+            Player1.SetActive(false);
+            Player2.SetActive(true);
+        }
+        else
+        {
+            PlayerSwitch = true;
+            Player1.SetActive(true);
+            Player2.SetActive(false);
+        }    
+    }
+}
