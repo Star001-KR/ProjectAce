21a8f20aec1348251aed84987b4f0f6e6f5dd305
김태형 <xo052345@gmail.com>
2020-12-17 15:00:09 +0900

    [기획/김태형] 그림자 회전 추가

--- ProjectAce/Assets/NewBehaviourScript.cs
+++ ProjectAce/Assets/NewBehaviourScript.cs
@@ -7,12 +7,23 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public GameObject Player1;
     public GameObject Player2;
+    public GameObject ShadowObject_Player1;
+    public GameObject ShadowObject_Player2;
     public Button UIButton;
     private bool PlayerSwitch;
-    
+    private float RotationShadow_Player1;
+    private float RotationShadow_Player2;
+    private float RotationSpeed_Player1;
+    private float RotationSpeed_Player2;
+
     // Start is called before the first frame update
     void Start()
     {
+        RotationShadow_Player1 = 0.0f;
+        RotationShadow_Player2 = 0.0f;
+        RotationSpeed_Player1 = 0.1f;
+        RotationSpeed_Player2 = 0.2f;
+
         Player1.SetActive(true);
         Player2.SetActive(false);
 
@@ -22,7 +33,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RotationShadow_Player1 += RotationSpeed_Player1;
+        RotationShadow_Player2 += RotationSpeed_Player2;
+
+        ShadowObject_Player1.transform.SetPositionAndRotation(ShadowObject_Player1.transform.position, Quaternion.Euler(new Vector3(0, RotationShadow_Player1, 0)));
+        ShadowObject_Player2.transform.SetPositionAndRotation(ShadowObject_Player2.transform.position, Quaternion.Euler(new Vector3(0, RotationShadow_Player2, 0)));
     }
 
     public void OnTextButtonCLick()
